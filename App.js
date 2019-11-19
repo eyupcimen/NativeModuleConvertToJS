f3b1deff039b3e48ec825d96498afd0e074d5d95
eyupcimen <cmneyp@gmail.com>
2019-11-19 14:26:27 +0300

    Create empty React Native project.

---
+++ App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+
+  state = {
+    count: 1
+  };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 })
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={[styles.wrapper, styles.border]}
+          onPress={this.increment}
+        >
+          <Text style={styles.button}>{this.state.count}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, alignItems: "stretch"
+  },
+  wrapper: {
+    flex: 1, alignItems: "center", justifyContent: "center"
+  },
+  border: {
+    borderColor: "#eee", borderBottomWidth: 1
+  },
+  button: {
+    fontSize: 50, color: "orange"
+  }
+});
